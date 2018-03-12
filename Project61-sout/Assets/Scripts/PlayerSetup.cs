3ca34e8cff50c95fa7527f7543959661f97ea9df
WalidLa <walid.lahyani20@gmail.com>
2018-03-12 16:05:39 +0100

    Multiplayer01

---
+++ Project61-sout/Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,55 @@
+﻿
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+	[SerializeField]
+	Behaviour[] componentsToDisable;
+
+	Camera sceneCamera;
+
+	void Start ()
+	{
+		// Disable components that should only be
+		// active on the player that we control
+		if (!isLocalPlayer) 
+		{
+			for (int i = 0; i < componentsToDisable.Length; i++) 
+			{
+				componentsToDisable [i].enabled = false;
+			}
+		} 
+		else 
+		{
+			sceneCamera = Camera.main;
+			if (sceneCamera != null) 
+			{
+				sceneCamera.gameObject.SetActive (false);
+			}
+		}
+	}
+
+	void OnDisable()
+	{
+
+		if (sceneCamera != null)
+		{
+			sceneCamera.gameObject.SetActive (true);
+		}
+	}
+	/*
+	void AssignRemoteLayer ()
+	{
+		gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
+	}
+
+	void DisableComponents ()
+	{
+		for (int i = 0; i < componentsToDisable.Length; i++)
+		{
+			componentsToDisable[i].enabled = false;
+		}
+	}*/
+}
+
