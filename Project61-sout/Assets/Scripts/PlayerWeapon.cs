bceacf0743fe00577a9e195e89398e4e70f9c37f
WalidLa <walid.lahyani20@gmail.com>
2018-04-22 22:14:19 +0200

    Fixed playershoot

--- Project61-sout/Assets/Scripts/PlayerWeapon.cs
+++ Project61-sout/Assets/Scripts/PlayerWeapon.cs
@@ -8,19 +8,20 @@ public class PlayerWeapon {
 	public int damage = 10;
 	public float range = 100f;
 
-	public float fireRate = 0f;
+	//public float fireRate = 0f;
 
-	public int maxBullets = 20;
-	[HideInInspector]
-	public int bullets;
+	//public int maxBullets = 20;
+	//[HideInInspector]
+	//public int bullets;
 
-	public float reloadTime = 1f;
+	//public float reloadTime = 1f;
 
-	public GameObject graphics;
+	//public GameObject graphics;
 
-	public PlayerWeapon ()
+	/*public PlayerWeapon ()
 	{
 		bullets = maxBullets;
 	}
+	*/
 
 }
