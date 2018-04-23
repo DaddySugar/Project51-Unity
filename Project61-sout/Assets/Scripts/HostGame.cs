88472783d98fe70b8b04aaf295f44313710cbe08
WalidLa <walid.lahyani20@gmail.com>
2018-04-23 15:24:36 +0200

    Lobby Host game

---
+++ Project61-sout/Assets/Scripts/HostGame.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostGame : MonoBehaviour
+{
+
+	[SerializeField]
+	private uint roomSize = 2; 
+	
+	
+	private string roomName;
+	// We can add pasword 
+	//private string roomPassword; 
+	private NetworkManager _networkManager;
+
+	private void Start()
+	{
+		_networkManager = NetworkManager.singleton;
+		if (_networkManager.matchMaker == null)
+		{
+			_networkManager.StartMatchMaker();
+		}
+	}
+
+	public void SetRoomName (string _name)
+	{
+		roomName = _name;
+	}
+
+	public void CreateRoom ()
+	{
+		if (roomName != "" && roomName != null)
+		{
+			Debug.Log("Creating Room: " + roomName + " with room for " + roomSize + " players.");
+			//We crete a room : with roomName + size 
+			_networkManager.matchMaker.CreateMatch(roomName, roomSize, true, "", "", "", 0, 0, _networkManager.OnMatchCreate);
+		}
+	}
+}
