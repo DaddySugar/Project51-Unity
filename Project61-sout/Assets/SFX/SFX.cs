9b77ef5636ad30969f7fc7c9059bd8cb38220d2a
MichelZiade <michel.ziade@epita.fr>
2018-04-24 14:30:28 +0200

    Sound

---
+++ Project61-sout/Assets/SFX/SFX.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFX : MonoBehaviour
+{
+
+    public AudioSource Beep;
+
+    public void PlayBeep()
+    {
+        Beep.Play();
+    }
+
+}
