f60919c75f5b0307505e55bd7ce2511db7908cd5
WalidLa <walid.lahyani20@gmail.com>
2018-04-25 16:36:57 +0200

    singleplayer attempt1

---
+++ Project61-sout/Assets/Scripts/HostSinglePlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostSinglePlayer : MonoBehaviour {
+
+	// We can add pasword 
+	//private string roomPassword; 
+	private NetworkManager _networkManager;
+
+	private void Start()
+	{
+		_networkManager = NetworkManager.singleton;
+		Debug.Log("3");
+		if (_networkManager.matchMaker == null)
+		{
+			Debug.Log("1");
+			_networkManager.StartMatchMaker();
+			Debug.Log("2");
+			
+		}
+	}
+
+
+	public void CreateRoom ()
+	{
+		Debug.Log("4");
+		_networkManager.matchMaker.CreateMatch("single", 1, true, "", "", "", 0, 0, _networkManager.OnMatchCreate);
+	}
+}
