5cccf617a453cdf368eb84546fa078ea97fbe665
PerninGuillaume <36130312+PerninGuillaume@users.noreply.github.com>
2018-04-27 16:49:42 +0200

    beginning alien ai

--- Project61-sout/Assets/alien/ChaseAlien.cs
+++ Project61-sout/Assets/alien/ChaseAlien.cs
@@ -4,9 +4,9 @@
 	using UnityEngine.AI;
 	using UnityEngine.Networking;
 	
-	public class ChaseAlien : NetworkBehaviour	
+	public class ChaseAlien : NetworkBehaviour
 	{
-	
+		private GameObject goal; 
 		//public GameObject player;
 		public Transform targetTransform; //the enemy's target
 		private NavMeshAgent agent;
@@ -22,21 +22,30 @@
 			anim = GetComponent<Animator>();
 			AlienPosition = GetComponent<Transform>();
 			raycastLayer = 2 << LayerMask.NameToLayer("LocalPlayer");
+			
+			//agent.destination =  NetworkManager.singleton.client.connection.playerControllers[0].gameObject.transform.position;
 		}
 		
 		void Update () 
 		{
-			SearchForTarget();
-			/*
-			agent.destination = player.transform.position;
-			if ((AlienPosition.position - player.transform.position).magnitude < 3)
+			//SearchForTarget();
+			
+			agent.destination = targetTransform.transform.position;
+			if (NetworkManager.singleton.client.connection.playerControllers.Count != 0)
+			{
+				agent.destination =  NetworkManager.singleton.client.connection.playerControllers[0].gameObject.transform.position;
+
+			}
+			Debug.Log(NetworkManager.singleton.client.connection.playerControllers.Count);
+
+			if ((AlienPosition.position - targetTransform.transform.position).magnitude < 3)
 			{
 				anim.SetBool("isAttacking", true);
 			}
 			else
 			{
 				anim.SetBool("isAttacking", false);
-			}*/
+			}
 	
 			
 		}
