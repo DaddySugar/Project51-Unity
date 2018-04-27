701dc6640b59591df90fa03fbdb0cf9dd74f7012
WalidLa <walid.lahyani20@gmail.com>
2018-04-27 17:16:03 +0200

    Spawn

--- Project61-sout/Assets/Scripts/WaveSpawner.cs
+++ Project61-sout/Assets/Scripts/WaveSpawner.cs
@@ -25,7 +25,7 @@ public class WaveSpawner : NetworkBehaviour {
 	 */
 	
 	
-	/*
+	
 	[System.Serializable]
 	public class Wave
 	{
@@ -187,55 +187,5 @@ public class WaveSpawner : NetworkBehaviour {
 		Transform _sp = spawnPoints[ Random.Range (0, spawnPoints.Length) ];
 		Instantiate(_enemy, _sp.position, _sp.rotation);
 	}
-*/
-	
-	
-	[SerializeField] GameObject zombiePrefab;
-	[SerializeField] GameObject Zombiepawn ;
-	private GameObject[] zombieSpawns;
-	private int counter;
-	private int numberOfZombies = 20;
-	private int maxNumberOfZombies = 80;
-	private float waveRate = 10;
-	private bool isSpawnActivated = true;
 
-	public override void OnStartServer ()
-	{
-		zombieSpawns = GameObject.FindGameObjectsWithTag("ZombieSpawn");
-		StartCoroutine(ZombieSpawner());
-	}
-
-	IEnumerator ZombieSpawner()
-	{
-		for(;;)
-		{
-			yield return new WaitForSeconds(waveRate);
-			GameObject[] zombies = GameObject.FindGameObjectsWithTag("Zombie");
-			if(zombies.Length < maxNumberOfZombies)
-			{
-				CommenceSpawn();
-			}
-		}
-	}
-
-	void CommenceSpawn()
-	{
-		if(isSpawnActivated)
-		{
-			for(int i = 0; i < numberOfZombies; i++)
-			{
-				int randomIndex = Random.Range(0, zombieSpawns.Length);
-				SpawnZombies(zombieSpawns[randomIndex].transform.position);
-			}
-		}
-	}
-
-	void SpawnZombies(Vector3 spawnPos)
-	{
-		
-		counter++;
-		GameObject go = GameObject.Instantiate(zombiePrefab, spawnPos, Quaternion.identity) as GameObject;
-		go.GetComponent<Alien_ID>().zombieID = "Zombie " + counter;
-		NetworkServer.Spawn(go);
-	}
 }
