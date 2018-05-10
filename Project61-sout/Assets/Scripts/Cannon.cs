5a2bc909de8c24e497184af68610e2079c87b576
WalidLa <walid.lahyani20@gmail.com>
2018-05-10 23:37:09 +0200

    Extra spaceships + working on visual effect

---
+++ Project61-sout/Assets/Scripts/Cannon.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Cannon : NetworkBehaviour {
+
+	[SerializeField] private int cost = 5000;
+	private int maxParts = 5;
+	[SyncVar]
+	public int currentpart;
+
+	private void Start()
+	{
+		currentpart = 0; 
+	}
+
+	void Update()
+	{
+		
+	}
+
+	
+
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.CompareTag("Player") && Input.GetKeyDown(KeyCode.E) && other.GetComponent<Player>().money >= cost && currentpart < maxParts)
+		{
+			StartCoroutine(Pickup(other));
+		}
+	}
+	
+
+	IEnumerator Pickup(Collider player)
+	{
+		Player stats = player.GetComponent<Player>();
+		currentpart++;
+		stats.money -= cost;
+		
+		yield return new WaitForSeconds(2);
+	}
+}
