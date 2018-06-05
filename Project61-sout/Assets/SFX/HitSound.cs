9b9531ed956bc2d9434e0b36f63c0ef66f1fe231
MichelZiade <michel.ziade@epita.fr>
2018-06-05 18:56:56 +0200

    Sound

---
+++ Project61-sout/Assets/SFX/HitSound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitSound : MonoBehaviour
+{
+    private float totalTimeBeforeDestroy9;
+
+    // Use this for initialization
+    void Start()
+    {
+        var sound = this.GetComponent<AudioSource>();
+        totalTimeBeforeDestroy9 = sound.clip.length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalTimeBeforeDestroy9 -= Time.deltaTime;
+
+        if (totalTimeBeforeDestroy9 <= 0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
